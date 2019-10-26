7b4ac86e1d372f637344928b2fb75d7e6d6473b1
Alexander Ewetumo <trinoxf@gmail.com>
2019-10-26 15:35:16 +0800

    UPT: Passed test for restack

--- restack_test.go
+++ restack_test.go
@@ -1,13 +1,106 @@
 package restark_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/influx6/restark"
 	"github.com/stretchr/testify/require"
 )
 
-func TestStack(t *testing.T) {
-	var stack restark.StackApp
-	require.Error(t, stack.Apply(1))
+func TestStackDefinitions(t *testing.T) {
+	var jsondef JSONDefinition
+	var res, err = restark.Define(func(obj *restark.DefStack) {
+		Object(obj, func() {
+			Name(obj, "Nature")
+			Desc(obj, "Desc")
+		})
+	})(&jsondef)
+
+	require.NoError(t, err)
+	require.NotNil(t, res)
+
+	require.NotNil(t, jsondef.Target)
+	require.Equal(t, jsondef.Target.Desc, "Desc")
+	require.Equal(t, jsondef.Target.Name, "Nature")
+}
+
+type JSONDefinition struct {
+	Target *Rob
+}
+
+func (j JSONDefinition) Elem() interface{} {
+	return j
+}
+
+func (j *JSONDefinition) Apply(v interface{}) error {
+	return nil
+}
+
+type Rob struct {
+	Name string
+	Desc string
+}
+
+func (j Rob) Elem() interface{} {
+	return j
+}
+
+func (j *Rob) Apply(v interface{}) error {
+	return nil
+}
+
+func Object(r *restark.DefStack, fn func()) {
+	var obj Rob
+
+	// Push object into task so functions
+	// can apply them to it.
+	r.Push(&obj)
+	fn()
+	// Pop object from stack, so we restore parent
+	// context
+	r.Pop()
+
+	var parent, err = r.Get()
+	if err != nil {
+		return
+	}
+
+	objParent, ok := parent.(*JSONDefinition)
+	if !ok {
+		r.SetErr(errors.New("parent is not an object type"))
+		return
+	}
+
+	objParent.Target = &obj
+}
+
+func Name(r *restark.DefStack, name string) {
+	var parent, err = r.Get()
+	if err != nil {
+		return
+	}
+
+	objParent, ok := parent.(*Rob)
+	if !ok {
+		r.SetErr(errors.New("parent is not an object type"))
+		return
+	}
+
+	objParent.Name = name
+}
+
+func Desc(r *restark.DefStack, desc string) {
+	var parent, err = r.Get()
+	if err != nil {
+		return
+	}
+
+	objParent, ok := parent.(*Rob)
+	if !ok {
+		r.SetErr(errors.New("parent is not an object type"))
+		return
+	}
+
+	objParent.Desc = desc
 }
--- restark.go
+++ restark.go
@@ -4,57 +4,110 @@ import (
 	"github.com/influx6/npkg/nerror"
 )
 
-// Stacked defines the base type which is to be applied to
+// Definition defines the base type which is to be applied to
 // given function.
-type Stacked interface {
+type Definition interface {
 	Elem() interface{}
-
 	Apply(interface{}) error
 }
 
-// StackApp manages a stack of Stacked implementing
+// DefStack manages a stack of Definition implementing
 // objects which allows popping and pushing value.
-type StackApp struct {
-	stacks []Stacked
+type DefStack struct {
+	err    error
+	stacks []Definition
 }
 
 // Apply applies provided value to the current top element
 // within provided stack.
-func (s *StackApp) Apply(v interface{}) error {
-	var current, err = s.Peek()
+func (s *DefStack) Apply(v interface{}) error {
+	if s.err != nil {
+		return s.err
+	}
+
+	var current, err = s.Get()
 	if err != nil {
 		return err
 	}
+
 	return current.Apply(v)
 }
 
-// Elem returns the StackApp which ensures it matches the stacked
+// SetErr sets the error returned when DefStack.Apply is
+// called.
+func (s *DefStack) SetErr(err error) {
+	s.err = err
+}
+
+// Elem returns the DefStack which ensures it matches the Definition
 // interface.
-func (s *StackApp) Elem() interface{} {
+func (s *DefStack) Elem() interface{} {
 	return s
 }
 
-// Push adds a new item into the stacked list.
-func (s *StackApp) Push(item Stacked) {
+// Push adds a new item into the Definition list.
+func (s *DefStack) Push(item Definition) {
 	s.stacks = append(s.stacks, item)
 }
 
-// Peek returns current Stacked object in stack.
-func (s *StackApp) Peek() (Stacked, error) {
+// Get returns current Definition object in stack.
+func (s *DefStack) Get() (Definition, error) {
 	if len(s.stacks) == 0 {
-		return nil, nerror.New("Empty stacked")
+		return nil, nerror.New("Empty Definition")
 	}
 	return s.stacks[len(s.stacks)-1], nil
 }
 
 // Pop pops recent stack to the last used stack.
 // If called iteratively then all items will be removed from stack.
-func (s *StackApp) Pop() (Stacked, error) {
+func (s *DefStack) Pop() Definition {
 	if len(s.stacks) == 0 {
-		return nil, nerror.New("Empty stacked")
+		return nil
 	}
 
 	elem := s.stacks[len(s.stacks)-1]
 	s.stacks = s.stacks[:len(s.stacks)-1]
-	return elem, nil
+	return elem
+}
+
+//**************************
+// Definition Functions
+//**************************
+
+// Definitions defines a function type which represents
+// an operation to be applied to a provided stack element.
+type Definitions func(stack *DefStack)
+
+// DefinitionApplication defines the function type which represents the
+// target for a definition function
+type DefinitionApplication func(source Definition) (Definition, error)
+
+// Define returns a function matching the DefinitionApplication type.
+// It applies all provided definitions to the source matching the Definition interface
+// which will host and validate definitions being applied to it.
+//
+// An error is returned if any definition (that is either the main or internals) fail
+// to meet defined criteria.
+func Define(fn ...Definitions) DefinitionApplication {
+	return func(source Definition) (Definition, error) {
+		// Create the Definition stack which will hold
+		// ever increasing definitions from child to parent contexts.
+		var stack DefStack
+		stack.Push(source)
+
+		// Apply definitions to stack.
+		for _, fn := range fn {
+			if stack.err != nil {
+				return source, stack.err
+			}
+
+			fn(&stack)
+		}
+
+		if stack.err != nil {
+			return source, stack.err
+		}
+
+		return source, nil
+	}
 }
